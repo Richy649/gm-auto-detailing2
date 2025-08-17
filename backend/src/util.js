51a915cbd6e53dca735f0a2861fc4a602374dc1c
Richy649 <gredvers1@gmail.com>
2025-08-17 11:50:10 +0100

    Create util.js

---
+++ backend/src/util.js
@@ -0,0 +1,10 @@
+export function toMinutes(hhmm){ const [h,m]=hhmm.split(':').map(Number); return h*60+m; }
+export function addMinutes(date, mins){ return new Date(new Date(date).getTime()+mins*60000); }
+export function clampToWorkingWindow(date, dayCfg){
+  const d = new Date(date);
+  const [sh,sm] = dayCfg.start.split(':').map(Number);
+  const [eh,em] = dayCfg.end.split(':').map(Number);
+  const start = new Date(d.getFullYear(), d.getMonth(), d.getDate(), sh, sm, 0, 0);
+  const end   = new Date(d.getFullYear(), d.getMonth(), d.getDate(), eh, em, 0, 0);
+  return { start, end };
+}
